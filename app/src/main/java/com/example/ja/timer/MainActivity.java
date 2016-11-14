f7353155b9b9b91f0fa20b72da23b39376c62729
szymongor <gornioczek.szymon@gmail.com>
2016-11-14 02:36:20 +0100

    Change et value

--- app/src/main/java/com/example/ja/timer/MainActivity.java
+++ app/src/main/java/com/example/ja/timer/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.ja.timer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn1 = (Button) findViewById(R.id.button4);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str = "1";
+                EditText et1 = (EditText) findViewById(R.id.editText7);
+                et1.setText(str, TextView.BufferType.EDITABLE);
+            }
+        });
+
     }
+
 }
